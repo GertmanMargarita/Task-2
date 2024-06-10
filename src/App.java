20fa9deec10acd3ffff49dbdd519215b0064556b
Margarita Gertman <margo3086027@gmail.com>
2024-06-10 18:53:21 +0300

    Start

---
+++ src/App.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+public class App {
+    public static int min (ArrayList<Integer> numbers) {	
+	    int min = Collections.min(numbers);
+	    return min;
+    }
+	
+    public static int max (ArrayList<Integer> numbers) {	
+	    int max = Collections.max(numbers);
+	    return max;
+    }
+	
+    public static BigInteger sum (ArrayList<Integer> numbers) {
+	    BigInteger sum = BigInteger.ZERO;
+	    for (int i = 0; i < numbers.size(); i++) {
+	   	    sum = sum.add(BigInteger.valueOf(numbers.get(i)));
+	    }
+	    return sum;
+    }
+	
+    public static BigInteger mult (ArrayList<Integer> numbers) {
+	    BigInteger mult = BigInteger.ONE;
+	    for (int i = 0; i < numbers.size(); i++) {
+		    mult = mult.multiply(BigInteger.valueOf(numbers.get(i)));
+   	    }
+   	    return mult;
+    }
+
+	public static void main(String[] args) {
+		String fileName = "D:\\numbers.txt";
+		File file = new File(fileName);
+		ArrayList<Integer> arrayA = new ArrayList<>();
+		try {
+			Scanner sc = new Scanner(file);
+			while (sc.hasNextInt()) {
+				int number = sc.nextInt();
+				
+				arrayA.add(number);
+				System.out.print(number + " ");
+			}
+			sc.close();			
+			System.out.println("Минимальное: " + min(arrayA));
+			System.out.println("Максимальное: " + max(arrayA));
+			System.out.println("Сумма: " + sum(arrayA));
+			System.out.println("Произведение: " + mult(arrayA));
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Ошибка чтения файла " + fileName);
+		}
+	}
+}
---
+++ src/AppTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testMin() {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        assertEquals(1, App.min(numbers));
+    }
+
+    @Test
+    public void testMax() {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        assertEquals(5, App.max(numbers));
+    }
+
+    @Test
+    public void testSum() {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        assertEquals(BigInteger.valueOf(15), App.sum(numbers));
+    }    
+
+    @Test
+    public void testMult() {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        assertEquals(BigInteger.valueOf(120), App.mult(numbers));
+    }
+    @Test 
+    public void testMultSpeed() {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i=0;i<10000000;i++){
+            numbers.add(i);
+        }
+        long sTime = System.currentTimeMillis();
+        App.mult(numbers);        
+        long fTime = System.currentTimeMillis();
+        
+        long xTime = fTime-sTime;
+        assertTrue("Time error: fuction mult execute more than 0,1 second",xTime <= 100);
+        
+    }
+
+}
